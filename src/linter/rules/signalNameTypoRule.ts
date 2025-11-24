9721267078a7475c77f97ab07448d721c6da9236
Jeff Verkoeyen <jeff@clutch.engineering>
2025-11-24 02:03:07 -0500

    Add Disconnector. (#28)

--- src/linter/rules/signalNameTypoRule.ts
+++ src/linter/rules/signalNameTypoRule.ts
@@ -36,7 +36,7 @@ export class SignalNameTypoRule implements ILinterRule {
     'coolant', 'antifreeze', 'brake', 'caliper', 'rotor', 'seatbelt',
     'airbag', 'traction', 'stability', 'cruise', 'parking', 'reverse',
     'overdrive', 'lockup', 'downshift', 'upshift', 'kickdown', 'recirculation',
-    'wastegate', 'evaporative', 'regen',
+    'wastegate', 'evaporative', 'regen', 'disconnector',
   ]);
 
   constructor() {
